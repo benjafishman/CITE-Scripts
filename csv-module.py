bed4114969cfbfc9d233d7e196e2c8dd8f800e5e
Ben-Zion Fishman <benjafishman@gmail.com>
2015-11-22 12:30:47 -0500

    - adding use of environment variables - a 'lil clean up

--- csv-module.py
+++ csv-module.py
@@ -5,28 +5,24 @@ import os
 import subprocess
 import shutil
 import dropbox
-
-SRC_DIR = '\\Users\\Ben\\Desktop\\CITE\\Testing\\'
-Processed_MP4_Dir = '\\Users\\Ben\\Desktop\\CITE\\Processed_MP4\\'
-Processed_MP3_Dir = '\\Users\\Ben\\Desktop\\CITE\\Processed_MP3\\'
-
-DropBoxFile = '/Test-Uploads/'
-
-csv_file_name = 'testcsv.csv'
+import Environment_Vars as ev
+ 
 Error = (0, 'No Errors')
 '''
 CSV Processing
 '''
-with open(csv_file_name, newline='') as csvfile:
+with open(ev.CSV_FILE_NAME, newline='') as csvfile:
     s = csv.reader(csvfile)
     for row in s:
-        # TODO: Add semester
+        # TODO: Add semester and check for each column var
+        # TODO: check env vars are set
         mp4_file_name = row[0]
         start_time = row[1]
         end_time = row[2]
+        DROPBOX_FILE_NAME = row[3]
 
         # Check if the file exists in the source directory
-        if not os.path.exists(SRC_DIR + mp4_file_name):
+        if not os.path.exists(ev.SRC_DIR + mp4_file_name):
             Error = (1, 'File does not exist')
         # Check if the file has valid extension
         elif len(mp4_file_name) < 4 or mp4_file_name[-4:] != '.mp4':
@@ -38,45 +34,54 @@ with open(csv_file_name, newline='') as csvfile:
             Convert mp4 to mp3 using ffmpeg command
             '''
             # Create the ffmpeg command string for mp4 to mp3 conversion
-            ffmpeg_convert_to_mp3_cmd = 'ffmpeg -i ' + SRC_DIR + mp4_file_name + ' ' + SRC_DIR + mp3_file_name
+            ffmpeg_convert_to_mp3_cmd = 'ffmpeg -i ' + ev.SRC_DIR + mp4_file_name + ' ' + ev.SRC_DIR + mp3_file_name
 
-            # Run the command
-            command_output = subprocess.check_output(ffmpeg_convert_to_mp3_cmd, shell=True)
+            try:
+                # Run the command
+                command_output = subprocess.check_output(ffmpeg_convert_to_mp3_cmd, shell=True)
+            except subprocess.CalledProcessError as e:
+                print(e)  
 
-            if not os.path.exists(Processed_MP3_Dir):
-                os.makedirs(Processed_MP3_Dir)
+            if not os.path.exists(ev.Processed_MP3_Dir):
+                os.makedirs(ev.Processed_MP3_Dir)
+
             '''
             Trim File according to csv specs
             '''
             # Create the ffmpeg trim command string and moved processed MP3 file to 'Processed_MP3_Dir' directory
-            trim_cmd = 'ffmpeg -ss ' + str(start_time) + ' -i ' + SRC_DIR + mp3_file_name + ' -to ' + str(
-                end_time) + ' ' + Processed_MP3_Dir + mp3_file_name
+            trim_cmd = 'ffmpeg -ss ' + str(start_time) + ' -i ' + ev.SRC_DIR + mp3_file_name + ' -to ' + str(
+                end_time) + ' ' + ev.Processed_MP3_Dir + mp3_file_name
 
             # Run the command
-            trim_command_output = subprocess.check_output(trim_cmd, shell=True)
+            try:
+                trim_command_output = subprocess.check_output(trim_cmd, shell=True)
+                # remove mp3 file from src directory
+                os.remove(ev.SRC_DIR + mp3_file_name)
+            except subprocess.CalledProcessError as e:
+                print(e)
 
             '''
             Move MP4 file to the 'Processed_MP4' directory
             '''
             # check if process_mp4 directory exists, if not make it
-            if not os.path.exists(Processed_MP4_Dir):
-                os.makedirs(Processed_MP4_Dir)
+            if not os.path.exists(ev.Processed_MP4_Dir):
+                os.makedirs(ev.Processed_MP4_Dir)
 
-            shutil.move(SRC_DIR + mp4_file_name, Processed_MP4_Dir + mp4_file_name)
+            shutil.move(ev.SRC_DIR + mp4_file_name, ev.Processed_MP4_Dir + mp4_file_name)
 
             '''
             Time to upload da file to DB BABAY!!
             '''
 
-            dbx = dropbox.Dropbox('')
+            dbx = dropbox.Dropbox(ev.DB_AUTH_KEY)
 
-            upload_file = Processed_MP3_Dir + mp3_file_name
+            upload_file = ev.Processed_MP3_Dir + mp3_file_name
 
             f = open(upload_file,'rb')
 
             print(f)
 
-            dbx.files_upload(f,DropBoxFile + mp3_file_name)
+            dbx.files_upload(f, "/" + DROPBOX_FILE_NAME + "/" + mp3_file_name)
 
             print('File upload complete?')
 
